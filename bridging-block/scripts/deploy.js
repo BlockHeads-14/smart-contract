e45e5331c52a2ac62ae7ec97c09451f90b4b37eb
Ferhad <mislim.sheridancollege.ca>
2023-09-12 20:05:01 -0400

    added the deploy script

---
+++ bridging-block/scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  //   console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Deploying the BridgingBlock contract
+  const BridgingBlock = await ethers.getContractFactory("BridgingBlock");
+  const bridgingBlock = await BridgingBlock.deploy();
+  await bridgingBlock.deployed();
+
+  console.log("BridgingBlock contract deployed to:", bridgingBlock.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
